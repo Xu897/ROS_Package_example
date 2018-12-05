92ada11532b06cdcbd48275d9c85068df1b2b6f5
Nguyen Duc Chien <nguyenchienbg2k@gmail.com>
2018-12-05 11:57:17 +0700

    Fix launcher

--- src/lane_detect/src/main.py
+++ src/lane_detect/src/main.py
@@ -1,5 +1,15 @@
 import rospy
 import cv2
+from cv_bridge import CvBridge
+
+TEAM="Team"
 
 
-# Code
+def callback(data):
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
+    cv2.imshow("View", cv_image)
+
+rospy.init_node('application')
+# rospy.Subscriber(TEAM + "_image", 1, callback)
+rospy.spin()
